1f6ea4f4f953db754092868cda95452a5d5408cc
Steellow <github@hanki.dev>
2025-06-27 18:57:58 +0300

    Add more example events

--- config/schedule.ts
+++ config/schedule.ts
@@ -19,6 +19,21 @@ const schedule: Gym[] = [
                 weekday: "Monday",
                 startTime: "10",
                 endTime: "16"
+            },
+            {
+                weekday: "Tuesday",
+                startTime: "12",
+                endTime: "18"
+            },
+            {
+                weekday: "Wednesday",
+                startTime: "09",
+                endTime: "15"
+            },
+            {
+                weekday: "Friday",
+                startTime: "14",
+                endTime: "20"
             }
         ]
     },
@@ -29,6 +44,46 @@ const schedule: Gym[] = [
                 weekday: "Monday",
                 startTime: "11",
                 endTime: "14"
+            },
+            {
+                weekday: "Wednesday",
+                startTime: "16",
+                endTime: "19"
+            },
+            {
+                weekday: "Thursday",
+                startTime: "10",
+                endTime: "13"
+            },
+            {
+                weekday: "Saturady",
+                startTime: "09",
+                endTime: "17"
+            }
+        ]
+    },
+    {
+        name: "Urban Movement",
+        shifts: [
+            {
+                weekday: "Tuesday",
+                startTime: "08",
+                endTime: "12"
+            },
+            {
+                weekday: "Thursday",
+                startTime: "15",
+                endTime: "21"
+            },
+            {
+                weekday: "Saturady",
+                startTime: "11",
+                endTime: "16"
+            },
+            {
+                weekday: "Sunday",
+                startTime: "13",
+                endTime: "18"
             }
         ]
     }
